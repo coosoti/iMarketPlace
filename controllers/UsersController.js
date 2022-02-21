a9d74a918464bfd2040d8c6e5cdb7dba786c6d6e
coosoti <charlesosoti@gmail.com>
2022-02-21 09:59:01 +0300

    [Feature - User Stats] implemented an endpoint to get user statistics

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,14 +1,14 @@
 import CryptoJS from 'crypto-js';
 import User from '../models/User';
 
-const CryptoSecKey = "kulundeng5victorbenosa";
+const CryptoSecKey = 'kulundeng5victorbenosa';
 
 class UsersController {
   static async verifyAuthToken(req, res) {
     if (req.body.password) {
       req.body.password = CryptoJS.AES.encrypt(
         req.body.password,
-        CryptoSecKey
+        CryptoSecKey,
       ).toString();
     }
 
@@ -18,7 +18,7 @@ class UsersController {
         {
           $set: req.body,
         },
-        { new: true }
+        { new: true },
       );
       res.status(200).json(updatedUser).end();
     } catch (err) {
@@ -30,8 +30,8 @@ class UsersController {
   static async delUser(req, res) {
     try {
       await User.findByIdAndDelete(req.params.id);
-      res.status(200).json("User has been deleted...").end();
-    } catch(err) {
+      res.status(200).json('User has been deleted...').end();
+    } catch (err) {
       res.status(500).json(err).end();
     }
   }
@@ -42,16 +42,45 @@ class UsersController {
       const user = await User.findById(req.params.id);
       const { password, ...others } = user._doc;
       res.status(200).json(others).end();
-    } catch(err) {
+    } catch (err) {
       res.status(500).json(err).end();
     }
   }
 
   // Get All Users
   static async getAllUsers(req, res) {
+    const query = req.query.new;
     try {
-      const users = await User.find();
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(5)
+        : await User.find();
       res.status(200).json(users).end();
+    } catch (err) {
+      res.status(500).json(err).end();
+    }
+  }
+
+  // Get User Stats
+  static async getUserStats(req, res) {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    try {
+      const data = await User.aggregate([
+        { $match: { createdAt: { $gte: lastYear } } },
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1},
+          },
+        },
+      ]);
+      res.status(200).json(data).end();
     } catch(err) {
       res.status(500).json(err).end();
     }
